3fd1f116e0521f5a501e40072f223c31c73b29a9
Marco Klaassen <marco.klaassen@lhind.dlh.de>
2020-10-09 18:24:16 +0200

    add metrics

--- src/main/java/click/klaassen/crosswind/CrosswindResource.java
+++ src/main/java/click/klaassen/crosswind/CrosswindResource.java
@@ -1,12 +1,15 @@
 package click.klaassen.crosswind;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.eclipse.microprofile.metrics.MetricUnits.MILLISECONDS;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 
 @Path("/crosswind")
 public class CrosswindResource {
@@ -15,7 +18,14 @@ public class CrosswindResource {
 
   @GET
   @Produces(APPLICATION_JSON)
-  public Crosswind hello(
+  @Counted(
+      name = "performedCrosswindCalculations",
+      description = "How many crosswind calculations have been performed.")
+  @Timed(
+      name = "crosswindCalculationTimer",
+      description = "A measure of how long it takes to calculate the crosswind.",
+      unit = MILLISECONDS)
+  public Crosswind calculateCrosswind(
       @QueryParam("runway") int runway,
       @QueryParam("wind_direction") int windDirection,
       @QueryParam("wind_knots") int windKnots) {
